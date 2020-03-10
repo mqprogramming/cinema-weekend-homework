9abf43d8f6ea3c2e4fdc5563e39231a61fbb4752
mqprogramming <matt.qgley@gmail.com>
2020-03-10 23:32:38 +0000

    further

--- controller.rb
+++ controller.rb
@@ -5,7 +5,12 @@ require_relative('./models/films.rb')
 also_reload('.models/all')
 
 get '/films' do
-  film_array = Film.all()
-  @film_titles = film_array.map { |film| film.title }
+  @film_array = Film.all()
   erb(:index)
+end
+
+get '/films/:film_id' do
+  @film_array = Film.all()
+  @found_film = @film_array.find { |film| film.id == params[:film_id].to_i }
+  erb(:film_page)
 end
